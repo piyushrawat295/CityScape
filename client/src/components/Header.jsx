ff43143e3d6e2c1f906c90fec7320c6cc3edb78b
Piyush Rawat <unexpected086@gmail.com>
2024-06-15 14:56:14 +0530

    SearchUI

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,22 +1,38 @@
-import React, { useState } from "react";
-import { Link, NavLink } from "react-router-dom";
+import React from "react";
+import { Link, NavLink, useNavigate } from "react-router-dom";
 import "./compo.css";
 import { useSelector } from "react-redux";
+import { useEffect, useState } from "react";
+import { FaSearch } from 'react-icons/fa';
 
 export default function Header() {
   const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+  const { currentUser } = useSelector((state) => state.user);
+  const [searchTerm, setSearchTerm] = useState("");
+  const navigate = useNavigate();
 
   const toggleMobileMenu = () => {
     setIsMobileMenuOpen(!isMobileMenuOpen);
   };
-  const { currentUser } = useSelector((state) => state.user);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const searchQuery = new URLSearchParams({ searchTerm }).toString();
+    navigate(`/search?${searchQuery}`);
+  };
+
+  useEffect(() => {
+    const urlParams = new URLSearchParams(window.location.search);
+    const searchTermFromUrl = urlParams.get("searchTerm");
+    if (searchTermFromUrl) {
+      setSearchTerm(searchTermFromUrl);
+    }
+  }, [location.search]);
+
   return (
     <nav className="border-gray-200 dark:bg-gray-900 sticky top-0 z-50 shadow-md">
       <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-3">
-        <Link
-          to="/"
-          className="flex items-center space-x-3 rtl:space-x-reverse"
-        >
+        <Link to="/" className="flex items-center space-x-3 rtl:space-x-reverse">
           <img
             src="https://flowbite.com/docs/images/logo.svg"
             className="h-8"
@@ -27,31 +43,19 @@ export default function Header() {
           </span>
         </Link>
         <div className="hidden md:flex md:items-center md:space-x-3">
-          <div className="relative">
+          <form onSubmit={handleSubmit} className="relative">
             <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
-              <svg
-                className="w-4 h-4 text-gray-500 dark:text-gray-400"
-                aria-hidden="true"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 20 20"
-              >
-                <path
-                  stroke="currentColor"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
-                />
-              </svg>
+              <FaSearch className="w-4 h-4 text-gray-500 dark:text-gray-400" />
             </div>
             <input
               type="text"
               id="search-navbar"
               className="block w-full p-2 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
               placeholder="Search..."
+              value={searchTerm}
+              onChange={(e) => setSearchTerm(e.target.value)}
             />
-          </div>
+          </form>
         </div>
         <div className="flex md:order-2">
           <button
@@ -87,61 +91,47 @@ export default function Header() {
         >
           <ul className="flex flex-col p-7 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
             <li className="md:hidden">
-              <div className="relative">
+              <form onSubmit={handleSubmit} className="relative">
                 <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
-                  <svg
-                    className="w-4 h-4 text-gray-500 dark:text-gray-400"
-                    aria-hidden="true"
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 20 20"
-                  >
-                    <path
-                      stroke="currentColor"
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth="2"
-                      d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
-                    />
-                  </svg>
+                  <FaSearch className="w-4 h-4 text-gray-500 dark:text-gray-400" />
                 </div>
                 <input
                   type="text"
                   id="search-navbar-mobile"
                   className="block w-full p-2 pl-11 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                   placeholder="Search..."
+                  value={searchTerm}
+                  onChange={(e) => setSearchTerm(e.target.value)}
                 />
-              </div>
+              </form>
             </li>
 
-            <li className="nav-item  text-xl">
+            <li className="nav-item text-xl">
               <NavLink className="nav-link" exact="true" to="/">
                 Home
               </NavLink>
             </li>
-            <li className="nav-item  text-xl">
+            <li className="nav-item text-xl">
               <NavLink className="nav-link" exact="true" to="/about">
                 About
               </NavLink>
             </li>
-            <li className="nav-item  text-xl">
+            <li className="nav-item text-xl">
               <NavLink className="nav-link" exact="true" to="/sign-in">
                 SignIn
               </NavLink>
-
-              
             </li>
             <Link to="/profile">
-                {currentUser ? (
-                  <img
-                    className="rounded-full h-10 w-10 object-cover"
-                    src={currentUser.avatar}
-                    alt="profile"
-                  />
-                ) : (
-                  <li className=" text-slate-700 hover:underline"> Sign in</li>
-                )}
-              </Link>
+              {currentUser ? (
+                <img
+                  className="rounded-full h-10 w-10 object-cover"
+                  src={currentUser.avatar}
+                  alt="profile"
+                />
+              ) : (
+                <li className="text-slate-700 hover:underline"> Sign in</li>
+              )}
+            </Link>
           </ul>
         </div>
       </div>
